d4560191ef49ab8d3b5e4612dfd5f9c4d4bf0d2d
Okubo <kazuki.okb@gmail.com>
2024-12-22 03:32:54 +0000

    📝 Topページにログインボタンとプレイヤーリストを追加

--- packages/frontend/src/components/top/main/battle-room-create/index.tsx
+++ packages/frontend/src/components/top/main/battle-room-create/index.tsx
@@ -1,6 +1,36 @@
-import { Card, CardContent, FormControl, Input, InputLabel, Typography } from '@mui/material';
+import login from '@/actions/login';
+import { useCurrentUser } from '@/hooks/use-curent-user';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  Input,
+  InputLabel,
+  Typography,
+} from '@mui/material';
 
 const BattleRoomCreate = () => {
+  const user = useCurrentUser;
+  let Btn;
+  if (user.email_verified) {
+    Btn = (
+      <Box sx={{ mt: 1 }}>
+        <Button fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+          CREATE ROOM
+        </Button>
+      </Box>
+    );
+  } else {
+    Btn = (
+      <Box component="form" action={() => login('google')} sx={{ mt: 1 }}>
+        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+          Sign In with Google
+        </Button>
+      </Box>
+    );
+  }
   return (
     <Card sx={{ flex: '1', width: '100%' }}>
       <CardContent>
@@ -10,6 +40,7 @@ const BattleRoomCreate = () => {
             テーマ名
           </InputLabel>
           <Input placeholder="お正月に余ったおせち食材の最適な使い方" />
+          {Btn}
         </FormControl>
       </CardContent>
     </Card>
